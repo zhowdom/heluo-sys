48c96fa9a2e386230e40f2e2fce0a854d03e2183
changfeng <changfeng@admin123.com>
2025-05-20 14:39:47 +0800

    fix: 打包配置修改；不生成新的js编译文件；资源路径修改

--- src/hooks/src/useUe.ts
+++ src/hooks/src/useUe.ts
@@ -8,41 +8,41 @@ export function useUeConnect() {
             param1: UeReportParams[UeReportType.FLOAT_DIRECTION],
             param2: payload?.opt
           })
-          window.ue.web.js_call_ue(UeReportParams[UeReportType.FLOAT_DIRECTION], payload?.opt)
+          // window.ue.web.js_call_ue(UeReportParams[UeReportType.FLOAT_DIRECTION], payload?.opt)
           break;
         case UeReportType.FLOAT_MENU_HOME:
           console.log({
             param1: UeReportParams[UeReportType.FLOAT_MENU_HOME],
             param2: payload?.opt
           })
-          window.ue.web.js_call_ue(UeReportParams[UeReportType.FLOAT_MENU_HOME], payload?.opt)
+          // window.ue.web.js_call_ue(UeReportParams[UeReportType.FLOAT_MENU_HOME], payload?.opt)
           break;
         case UeReportType.WEATHER:
           console.log({
             param1: UeReportParams[UeReportType.WEATHER]
           })
-          window.ue.web.js_call_ue(UeReportParams[UeReportType.WEATHER])
+          // window.ue.web.js_call_ue(UeReportParams[UeReportType.WEATHER])
           break;
         case UeReportType.WARN_ITEM:
           console.log({
             param1: UeReportParams[UeReportType.WARN_ITEM],
             param2: payload?.opt
           })
-          window.ue.web.js_call_ue(UeReportParams[UeReportType.WARN_ITEM], payload?.opt)
+          // window.ue.web.js_call_ue(UeReportParams[UeReportType.WARN_ITEM], payload?.opt)
           break;
         case UeReportType.PROFESSION:
           console.log({
             param1: UeReportParams[UeReportType.PROFESSION],
             param2: payload?.opt
           })
-          window.ue.web.js_call_ue(UeReportParams[UeReportType.PROFESSION], payload?.opt)
+          // window.ue.web.js_call_ue(UeReportParams[UeReportType.PROFESSION], payload?.opt)
           break;
         case UeReportType.FLOOR:
           console.log({
             param1: UeReportParams[UeReportType.FLOOR],
             param2: payload?.opt
           })
-          window.ue.web.js_call_ue(UeReportParams[UeReportType.FLOOR], payload?.opt)
+          // window.ue.web.js_call_ue(UeReportParams[UeReportType.FLOOR], payload?.opt)
           break;
         default:
           console.log('No matched to event type')
