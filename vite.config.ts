ac1e0850210f33291eef9b03ef06b2ddbe51c631
changfeng <changfeng@admin123.com>
2025-05-20 17:34:24 +0800

    feat: 修复打包后dist空白资源错误问题;

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from 'path' // need install @types/node
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ["ie>11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], //解决跨域警告
+    })
+  ],
+  base: './',
   build: {
+    target: ['es2015', 'chrome63'], // 默认是modules,百度说是更改这个会去输出兼容浏览器
     assetsInlineLimit: 4096 // < 4KB的文件转base64
   },
-  // base: "./", // 相对路径[打包后空白]
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
